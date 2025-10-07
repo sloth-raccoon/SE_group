be4cf28b769859aeb82fc747fb8c3cf311d348e6
s112213063\user <s112213063@mail1.ncnu.edu.tw>
2025-10-07 10:46:07 +0800

    add divide function

--- math.py
+++ math.py
@@ -1,3 +1,8 @@
+def divide(a, b):
+    if b == 0:
+        return "Error: Cannot divide by zero."
+     return a / b
+
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
 choice = input("Enter choice (+,-,*,/): ")
@@ -12,3 +17,4 @@ elif choice == '/':
     print(f"Result: {divide(num1, num2)}")
 else:
     print("Invalid input")
+    
